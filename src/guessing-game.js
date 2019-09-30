57501ed7587af8ddfe780af3af8d03b3580579ec
InvisibleSpectator <vladislav5678@yandex.ru>
2019-09-30 19:41:42 +0400

    feat: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,21 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.candidate = Math.round((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.setRange(this.min, this.candidate);
     }
 
     greater() {
-
+        this.setRange(this.candidate, this.max);
     }
 }
 
